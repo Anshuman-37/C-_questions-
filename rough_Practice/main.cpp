2c725177cf104aedb0b098f956e1d7f09e9b0111
Anshuman <asingh99cool@gmail.com>
2019-12-01 20:21:25 +0530

    XD

--- rough_Practice/main.cpp
+++ rough_Practice/main.cpp
@@ -9,101 +9,135 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-
 using namespace std;
-void merge(int arr[], int l, int m, int r)
+void merge(int arr[],int start,int mid,int end)
 {
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 =  r - m;
-  
-    /* create temp arrays */
-    int L[n1], R[n2];
-  
-    /* Copy data to temp arrays L[] and R[] */
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
-  
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
-    while (i < n1 && j < n2)
+    int n1 = mid - start + 1;
+    int n2 = end - mid;
+    
+    int *ar1 = new int[n1];
+    int *ar2 = new int[n2];
+    
+    for(int i = 0 ;i < n1 ; i++)
     {
-        if (L[i] <= R[j])
+        ar1[i] = arr[start+i];
+    }
+    
+    for(int j = 0 ; j < n2 ; j++)
+    {
+        ar2[j] = arr[mid+1+j];
+    }
+    
+    int i = 0 , j = 0 ,k = start;
+    while(i < n1 && j < n2)
+    {
+        if(ar1[i] < ar2[j])
         {
-            arr[k] = L[i];
+            arr[k] = ar1[i];
             i++;
         }
-        else
+        else if(ar2[j] < ar1[i])
         {
-            arr[k] = R[j];
+            arr[k] = ar2[j];
             j++;
         }
         k++;
     }
-  
-    /* Copy the remaining elements of L[], if there
-       are any */
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
+    
+    while(i<n1){
+        arr[k] = ar1[i];
+        i++; k++;
     }
-  
-    /* Copy the remaining elements of R[], if there
-       are any */
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
+    while(j<n2){
+        arr[k] = ar2[j];
+        j++;k++;
     }
 }
-  
-/* l is for left index and r is right index of the
-   sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r)
+void merge_sort(int arr[],int start,int end)
 {
-    if (l < r)
-    {
-        // Same as (l+r)/2, but avoids overflow for
-        // large l and h
-        int m = l+(r-l)/2;
-  
-        // Sort first and second halves
-        mergeSort(arr, l, m);
-        mergeSort(arr, m+1, r);
-  
-        merge(arr, l, m, r);
+    if(start < end){
+        int mid = (start+end)/2;
+        
+        merge_sort(arr,start,mid);
+        merge_sort(arr, mid+1, end);
+        merge(arr,start,mid,end);
+    }
+    
+    return;
+}
+void bubble_sort(int arr[],int n)
+{
+    int i = 0 , j = 0;
+    
+    for(i = 0; i < n - 1;i++){
+        for(j = i ; j < n ; j++)
+        {
+            if(arr [i] >= arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                
+            }
+        }
     }
 }
-  
-/* UTILITY FUNCTIONS */
-/* Function to print an array */
-void printArray(int A[], int size)
+void insertion_sort(int arr[], int n)
 {
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", A[i]);
-    printf("\n");
+    int i, key, j;
+      for (i = 1; i < n; i++)
+      {
+          key = arr[i];
+          j = i - 1;
+    
+          while (j >= 0 && arr[j] > key)
+          {
+              arr[j + 1] = arr[j];
+              j = j - 1;
+          }
+          arr[j + 1] = key;
+      }
 }
-  
-/* Driver program to test above functions */
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+int Partition(int arr[] ,int low ,int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+     
+       for (int j = low; j <= high - 1; j++)
+       {
+           if (arr[j] < pivot)
+           {
+               i++;
+               swap(&arr[i], &arr[j]);
+           }
+       }
+       swap(&arr[i + 1], &arr[high]);
+       return (i + 1);
+}
+void quick_sort(int arr[] , int low ,int high)
+{
+    if(low < high){
+       int  pi = Partition(arr, low, high);
+        
+        quick_sort(arr , low , pi-1);
+        quick_sort(arr , pi+1 , high);
+    }
+    
+    return;
+}
 int main()
 {
     int arr[] = {12, 11, 13, 5, 6, 7};
-    int arr_size = sizeof(arr)/sizeof(arr[0]);
-  
-    printf("Given array is \n");
-    printArray(arr, arr_size);
-  
-    mergeSort(arr, 0, arr_size - 1);
-  
-    printf("\nSorted array is \n");
-    printArray(arr, arr_size);
+    int n = sizeof(arr)/sizeof(arr[0]);
+    quick_sort(arr , 0 , n - 1);
+    for(auto i : arr)
+    {
+        cout<<i<<" ";
+    }
     return 0;
-} 
+}

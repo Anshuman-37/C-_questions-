7cd8ad2f79a1fd5c5e3b3e41513c9756b0b007d1
Anshuman <asingh99cool@gmail.com>
2020-03-24 10:11:56 +0530

    This is for finding duplicates in array in O(1) space with O(n) time

---
+++ Something_new/Something_new/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Something_new
+//
+//  Created by Anshuman  on 08/12/19.
+//  Copyright © 2019 Anshuman . All rights reserved.
+//
+
+import Foundation
+var myvar = 50
+
+print("Hello, World! ",myvar)
+
+
--- rough_Practice/main.cpp
+++ rough_Practice/main.cpp
@@ -1,143 +1,60 @@
-//
-//  main.cpp
-//  rough_Practice
-//
-//  Created by Anshuman  on 24/11/19.
-//  Copyright © 2019 Anshuman . All rights reserved.
-//
-
 #include <iostream>
-#include <algorithm>
+#include <cstdlib>
 #include <vector>
 using namespace std;
-void merge(int arr[],int start,int mid,int end)
+void get_duplicate(int* arr, int n)
 {
-    int n1 = mid - start + 1;
-    int n2 = end - mid;
-    
-    int *ar1 = new int[n1];
-    int *ar2 = new int[n2];
-    
-    for(int i = 0 ;i < n1 ; i++)
+        if(n == 0)
+            exit(0);
+    for(int i = 0 ; i < n ; i++)
     {
-        ar1[i] = arr[start+i];
-    }
-    
-    for(int j = 0 ; j < n2 ; j++)
-    {
-        ar2[j] = arr[mid+1+j];
-    }
-    
-    int i = 0 , j = 0 ,k = start;
-    while(i < n1 && j < n2)
-    {
-        if(ar1[i] < ar2[j])
+        if(arr[abs(arr[i])] >= 0)
         {
-            arr[k] = ar1[i];
-            i++;
+        arr[abs(arr[i])] = -arr[abs(arr[i])];
         }
-        else if(ar2[j] < ar1[i])
-        {
-            arr[k] = ar2[j];
-            j++;
-        }
-        k++;
+        else
+            cout<<abs(arr[i]) <<" ";
     }
-    
-    while(i<n1){
-        arr[k] = ar1[i];
-        i++; k++;
-    }
-    while(j<n2){
-        arr[k] = ar2[j];
-        j++;k++;
-    }
+
 }
-void merge_sort(int arr[],int start,int end)
+int findDuplicate3(vector<int>& nums)
 {
-    if(start < end){
-        int mid = (start+end)/2;
-        
-        merge_sort(arr,start,mid);
-        merge_sort(arr, mid+1, end);
-        merge(arr,start,mid,end);
-    }
-    
-    return;
-}
-void bubble_sort(int arr[],int n)
-{
-    int i = 0 , j = 0;
-    
-    for(i = 0; i < n - 1;i++){
-        for(j = i ; j < n ; j++)
+    if (nums.size() > 1)
         {
-            if(arr [i] >= arr[j]){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast)
+            {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
             }
-        }
-    }
-}
-void insertion_sort(int arr[], int n)
-{
-    int i, key, j;
-      for (i = 1; i < n; i++)
-      {
-          key = arr[i];
-          j = i - 1;
-    
-          while (j >= 0 && arr[j] > key)
-          {
-              arr[j + 1] = arr[j];
-              j = j - 1;
-          }
-          arr[j + 1] = key;
-      }
-}
-void swap(int* a, int* b)
-{
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-int Partition(int arr[] ,int low ,int high)
-{
-    int pivot = arr[high];
-    int i = (low - 1);
-     
-       for (int j = low; j <= high - 1; j++)
-       {
-           if (arr[j] < pivot)
-           {
-               i++;
-               swap(&arr[i], &arr[j]);
-           }
-       }
-       swap(&arr[i + 1], &arr[high]);
-       return (i + 1);
-}
-void quick_sort(int arr[] , int low ,int high)
-{
-    if(low < high){
-       int  pi = Partition(arr, low, high);
         
-        quick_sort(arr , low , pi-1);
-        quick_sort(arr , pi+1 , high);
-    }
-    
-    return;
+        fast = 0;
+        while (fast != slow)
+            {
+            fast = nums[fast];
+            slow = nums[slow];
+            }
+        return slow;
+        }
+    return -1;
 }
 int main()
 {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    quick_sort(arr , 0 , n - 1);
-    for(auto i : arr)
+    int n;
+    cin>>n;
+    int* arr = new int[n];
+    for(int i = 0 ; i < n; i++)
     {
-        cout<<i<<" ";
+        cin>>arr[i];
     }
+    vector<int> v1;
+    for(int i = 0 ; i < n ; i++)
+    {
+        v1[i] = arr[i];
+    }
+    findDuplicate3(v1);
+   // get_duplicate(arr,n); // Will get the duplicates in the array
     return 0;
+
 }
